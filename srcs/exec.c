7110b39140337f40f9eb6e087c05c32bd7d6f403
Ken Kamata <kn12kamata@gmail.com>
2021-09-18 12:44:37 +0900

    Divide parse path func

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: kkamata <kkamata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 20:49:20 by kkamata           #+#    #+#             */
-/*   Updated: 2021/09/18 10:31:51 by kkamata          ###   ########.fr       */
+/*   Updated: 2021/09/18 12:38:03 by kkamata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,12 @@ static void	free_paths(char **paths)
 	free_util(paths);
 }
 
-char	*find_path(char *cmd, char **envp)
+static char	*find_command(char *cmd, char **paths)
 {
-	char	**paths;
 	char	*path;
 	char	*tmp;
 	size_t	i;
 
-	(void)cmd;
-	i = 0;
-	while (!ft_strstr(envp[i], "PATH"))
-		i++;
-	paths = ft_split(envp[i] + 5, ':');
-	if (!paths)
-		exit(error_byname("ft_split"));
 	i = 0;
 	while (paths[i])
 	{
@@ -58,6 +50,20 @@ char	*find_path(char *cmd, char **envp)
 	return (NULL);
 }
 
+static char	*parse_path(char *cmd, char **envp)
+{
+	char	**paths;
+	size_t	i;
+
+	i = 0;
+	while (!ft_strstr(envp[i], "PATH"))
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	if (!paths)
+		exit(error_byname("ft_split"));
+	return (find_command(cmd, paths));
+}
+
 void	exec_cmd(char *argv, char *envp[])
 {
 	char	**cmd;
@@ -68,7 +74,7 @@ void	exec_cmd(char *argv, char *envp[])
 	if (ft_strchr(cmd[0], '/'))
 		if (execve(cmd[0], &cmd[0], envp) == -1)
 			exit(error_notcmd());
-	if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
+	if (execve(parse_path(cmd[0], envp), cmd, envp) == -1)
 		exit(error_notcmd());
 }
 
