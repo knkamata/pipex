a194547ed4d3c1178ffbce939dfe9e5a740e44a6
Ken Kamata <kn12kamata@gmail.com>
2021-09-23 16:55:49 +0900

    Modify error messages

--- includes/msgs.h
+++ includes/msgs.h
@@ -6,7 +6,7 @@
 /*   By: kkamata <kkamata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 21:31:02 by kkamata           #+#    #+#             */
-/*   Updated: 2021/09/18 08:02:18 by kkamata          ###   ########.fr       */
+/*   Updated: 2021/09/23 16:15:32 by kkamata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 // +------------------------------------------+ //
 //   Error messages                             //
 // +------------------------------------------+ //
-# define ERRMSG "Error"
-# define ERRMSG_ARGC "Error: Invalid number of arguments"
+
+# define ERRMSG_ARGC "Error: invalid number of arguments"
 # define ERRMSG_USAGE1 "Usage: ./pipex <file1> <cmd1> <cmd2> ... <cmdn> <file2>"
 # define ERRMSG_USAGE2 "Usage: ./pipex here_doc LIMITER <cmd1> <cmd2> <file>"
-# define ERRMSG_NOTCMD "Error: Command not found"
-# define ERRMSG_NOFILE "Error: No such file or directory"
+# define ERRMSG_NOTCMD "Error: command not found: "
+# define ERRMSG_NOFILE "Error: no such file or directory: "
 
 #endif
