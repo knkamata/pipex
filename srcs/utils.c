a765bd7ea2b11c4d10e55b4030dd41dfc4d003d6
Ken Kamata <kn12kamata@gmail.com>
2021-09-23 17:00:33 +0900

    Modify close util function

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,16 +6,22 @@
 /*   By: kkamata <kkamata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 09:01:07 by kkamata           #+#    #+#             */
-/*   Updated: 2021/09/18 09:42:46 by kkamata          ###   ########.fr       */
+/*   Updated: 2021/09/23 15:57:38 by kamata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	close_util(int fd)
+static void	close_fd(int fd)
 {
 	if (close(fd) == -1)
 		exit(error_byname("close"));
+}
+
+void	close_util(int fd[2])
+{
+	close_fd(fd[0]);
+	close_fd(fd[1]);
 }
 
 void	dup2_util(int fd1, int fd2)
